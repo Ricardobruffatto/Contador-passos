32aa090a9231ec9480d91e8fcf5f37cfe292d409
Ricardo <bruffatto70@gmail.com>
2025-05-03 17:27:00 -0300

    commit final

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-function clicar() {
+function calcular() {
     var txti = document.getElementById('txti')
     var txtf = document.getElementById('txtf')
     var txtp = document.getElementById('txtp')
     var res = document.getElementById('res')
     if (txti.value.length == 0 || txtf.value.length == 0 || txtp.value.length == 0) {
         alert('Entrada inválida')
-    } else{
+    } else {
         res.innerHTML = `Contando..`
         let inicio = Number(txti.value)
         let fim = Number(txtf.value)
         let passo = Number(txtp.value)
 
-        for(let contador = inicio; inicio<=fim; contador += passo){
-            res.innerHTML += `${contador}`
+        if(passo<=0){
+            window.alert('Passo inválido! Considerando passo 1')
+            passo = 1
         }
+
+        if (inicio < fim) {
+
+            //contagem crescente
+            for (let contador = inicio; contador <= fim; contador += passo) {
+                res.innerHTML += `${contador} `
+            }
+
+        } else {
+            //contagem decrescente
+            for(let contador = inicio; contador>=fim; contador-=passo){
+                res.innerHTML = `${c} \u{1F449}`
+            }
+        }
+
 
 
     }
